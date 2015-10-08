a868f3626715c663bf676e5d1ee3797c1747104a
yenniejun <yennie.jun@gmail.com>
2015-10-08 15:37:33 -0400

    background gif on image canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,19 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	// Assume that the content is displayed in broswers 
+	// that DO support <canvas>
+
+	var img = new Image();
+	img.src = "duckhunt-background.gif";
+
+	ctx.drawImage(img, 0, 0, 600, 800);
+
+	// Draw Slice
+	//ctx.drawImage(img, 50, 50, 50, 50,
+	//	50, 50, 50, 50);
+
+	// Draw frame
+//	ctx.drawImage(img, 0, 0);
+
+}
