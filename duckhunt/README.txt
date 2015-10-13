c5adabe6962d6b84bd0d64a5cfed86233c5c804b
yenniejun <yennie.jun@gmail.com>
2015-10-13 13:55:24 -0400

    Adding README

---
+++ duckhunt/README.txt
@@ -0,0 +1,7 @@
+README.txt
+Yennie Jun
+October 13, 2015
+Lab 5 
+
+I correctly drew the background and the birds on the background.
+I spent 2-3 hours on this assignment.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -6,25 +6,17 @@ function init() {
 
 	var img = new Image();
 	img.addEventListener("load", function() {
+		// drawing the background image
 		ctx.drawImage(img, 0, 0, 512, 480);
-		var birds = new Image();
-		birds.src = "duckhunt_various_sheet.png"
+		// slicing and drawing first bird
 		ctx.drawImage(birds, 42, 117, 38, 30, 175, 140, 80, 60);
+		// slicing and drawing second bird
 		ctx.drawImage(birds, 125, 120, 38, 30, 300, 100, 80, 60);
 	}, false);
 
 
 	img.src = "duckhunt-background.gif";
-
-	// drawing the background image
-	ctx.drawImage(img, 0, 0, 512, 480);
-
-	// slicing and drawing first bird
 	var birds = new Image();
-	birds.src = "duckhunt_various_sheet.png"
-	ctx.drawImage(birds, 42, 117, 38, 30, 175, 140, 80, 60);
-
-	// slicing and drawing second bird
-	ctx.drawImage(birds, 125, 120, 38, 30, 300, 100, 80, 60);
+	birds.src = "duckhunt_various_sheet.png";
 
 }
