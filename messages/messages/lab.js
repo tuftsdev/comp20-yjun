7c6f0d92b01a7f932031796f44130cabf45b169a
yenniejun <yennie.jun@gmail.com>
2015-10-15 15:53:35 -0400

    Trying to open json file

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,1 +1,15 @@
-// Your JavaScript goes here...
+function parse () {
+	console.log("YO");
+	var xmlhttp = new XMLHttpRequest();
+	var url = "data.json"
+	xmlhttp.open("GET", url, true);
+
+	var jsondata = JSON.parse(xmlhttp.responseText);
+/*	for (var i = 0; i < jsondata.length; i++) {
+		console.log ("HEY");
+	}
+*/
+
+
+};
+
