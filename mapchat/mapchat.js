a85eb882f5115c58bda76960b3bda9f50bed41ba
yenniejun <yennie.jun@gmail.com>
2015-10-18 12:51:35 -0400

    Getting rid of separate js file

---
+++ mapchat/mapchat.js
@@ -0,0 +1,12 @@
+function init() {
+
+	var myLat = 0;
+	var myLng = 0;
+
+	// making request
+	var request = new XMLHttpRequest();
+	var myLoc = new google.maps.LatLng(myLat, my Lng);
+
+	console.log (myLoc);
+
+}
