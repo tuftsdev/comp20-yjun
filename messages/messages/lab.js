cc6ca26423b76e01c6717d8b099211d225bd3709
yenniejun <yennie.jun@gmail.com>
2015-10-17 21:49:51 -0400

    moving stuff

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -4,17 +4,17 @@ function parse() {
 
 	console.log("YOOOO");
 
+
+
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-
-
+		xmlhttp.open("GET", url,true );
+		xmlhttp.send();
 		console.log(xmlhttp.response);
 		var myArr = JSON.parse(xmlhttp.responseText);
 		console.log("Done parsing?");
 		console.log(myArr);
 	}
 
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
 	
 
 }
