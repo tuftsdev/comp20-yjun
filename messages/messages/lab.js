6794515cecfda7fe7c47b05841238bb92b19a315
yenniejun <yennie.jun@gmail.com>
2015-10-17 21:51:01 -0400

    Tryna

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -5,11 +5,12 @@ function parse() {
 	console.log("YOOOO");
 
 
-xmlhttp.open("GET", url,true );
-		xmlhttp.send();
+	xmlhttp.open("GET", url,true );
+	xmlhttp.send();
+			console.log(xmlhttp.response);
+
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 		
-		console.log(xmlhttp.response);
 		var myArr = JSON.parse(xmlhttp.responseText);
 		console.log("Done parsing?");
 		console.log(myArr);
