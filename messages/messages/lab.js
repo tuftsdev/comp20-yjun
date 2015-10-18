48d9c5d90aa6b618c5a81de4424e0aedcd22fa8c
yenniejun <yennie.jun@gmail.com>
2015-10-17 21:52:33 -0400

    non synchronous?

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,13 +1,12 @@
 function parse() {
 	var xmlhttp = new XMLHttpRequest();
-	var url = "data.json";
 
 	console.log("YOOOO");
 
 
-	xmlhttp.open("GET", url,true );
+	xmlhttp.open("GET", "data.json", false);
 	xmlhttp.send();
-			console.log(xmlhttp.response);
+	console.log(xmlhttp.response);
 
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 		
