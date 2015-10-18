a8e21725520b32bf9c38a9d57cbab58d8276c8a3
yenniejun <yennie.jun@gmail.com>
2015-10-17 21:41:41 -0400

    Trying to parse.

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,1 +1,11 @@
-// Your JavaScript goes here...
+function parse() {
+	var xmlhttp = new XMLHttpRequest();
+	var url = "data.json";
+	console.log("YOOOO");
+
+	var myArr = JSON.parse(xmlhttp.response.Text);
+	
+	console.log("Done parsing?");
+	console.log(myArr);
+
+}
