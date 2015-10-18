3b1e82dcf27e29663a46021d07be3aa3d82c5f60
yenniejun <yennie.jun@gmail.com>
2015-10-17 21:48:45 -0400

    Changed syntax

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,14 +1,20 @@
 function parse() {
 	var xmlhttp = new XMLHttpRequest();
 	var url = "data.json";
+
 	console.log("YOOOO");
 
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+
 		console.log(xmlhttp.response);
-		var myArr = JSON.parse(xmlhttp.response.Text);
+		var myArr = JSON.parse(xmlhttp.responseText);
 		console.log("Done parsing?");
 		console.log(myArr);
 	}
+
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 	
 
 }
