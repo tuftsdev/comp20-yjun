fc71ad6f9a961d7e13d2c4f4ca0361e72254e5f6
yenniejun <yennie.jun@gmail.com>
2015-10-17 22:07:24 -0400

    Adding parsed obj

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -6,9 +6,15 @@ function parse() {
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState === 4) {
 			console.log("CHEESE");
-			var parser = JSON.parse(xmlhttp.responseText);
-			console.log(parser);
+			var parsed = JSON.parse(xmlhttp.responseText);
+			console.log(parsed);
 			//console.log(xmlhttp.responseText);
+
+			var elem = document.getElementById("messages");
+			for (var i = 0; i < parsed.length; count++) {
+				console.log(Object.keys(parsedObjects[count]));
+
+			}
 		}
 
 
