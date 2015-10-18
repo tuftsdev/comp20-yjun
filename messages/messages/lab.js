35e13553d8ae46f95526f2895fd2f22b84097863
yenniejun <yennie.jun@gmail.com>
2015-10-17 23:11:43 -0400

    Doing blocks

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,20 +1,16 @@
 function parse() {
 	var xmlhttp = new XMLHttpRequest();
 
-	console.log("YOOOO");
-
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState === 4) {
-			console.log("CHEESE");
 			var parsed = JSON.parse(xmlhttp.responseText);
 			console.log(parsed);
 			//console.log(xmlhttp.responseText);
 
 			var elem = document.getElementById("messages");
 			for (var count = 0; count < parsed.length; count++) {
-				console.log(Object.keys(parsed[count]));
 				elem.innerHTML += "<p>" + parsed[count]["content"] 
-					+ " " + parsed[count]["username"] + "</p>";
+					+ "    " + parsed[count]["username"] + "</p>";
 			}
 		}
 
@@ -24,7 +20,4 @@ function parse() {
 	//asynchronous
 	xmlhttp.open("GET", "data.json", true);
 	xmlhttp.send();
-	console.log("YAY");
-
-
 }
