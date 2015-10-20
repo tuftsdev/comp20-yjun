e99fa4eb6b2fb3db4b48fcd01f832d3dc290ab04
yenniejun <yennie.jun@gmail.com>
2015-10-20 14:46:46 -0400

    Parsing and printing correctly

--- messages/messages/lab.js
+++ messages/messages/lab.js
@@ -1,15 +1,26 @@
 function parse () {
-	console.log("YO");
 	var xmlhttp = new XMLHttpRequest();
 	var url = "data.json"
+
+	// Step 1: Set up HTTP request
 	xmlhttp.open("GET", url, true);
 
-	var jsondata = JSON.parse(xmlhttp.responseText);
-/*	for (var i = 0; i < jsondata.length; i++) {
-		console.log ("HEY");
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var jsondata = JSON.parse(xmlhttp.responseText);
+			elem = document.getElementById("messages");
+
+		for (var count = 0; count < jsondata.length; count++) {
+			elem.innerHTML += "<p>" + jsondata[count]["content"] +  "     " + 
+				"<span>" + jsondata[count]["username"] + "</span>" + "</p>";
+			}
+		}
 	}
-*/
 
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 
-};
+	
+
+}
 
